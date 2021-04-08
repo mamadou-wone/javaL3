ae6c0b1aaf1a2f6e7e2c4429f63d0564e49d2f80
mwone472 <mamadouwone12345@gmail.com>
2021-04-08 22:42:50 +0000

    08/04/2021

--- Hopital/Medicament.java
+++ Hopital/Medicament.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
+//   ___       __   ________  ________   _______      
+//  |\  \     |\  \|\   __  \|\   ___  \|\  ___ \     
+//  \ \  \    \ \  \ \  \|\  \ \  \\ \  \ \   __/|    
+//   \ \  \  __\ \  \ \  \\\  \ \  \\ \  \ \  \_|/__  
+//    \ \  \|\__\_\  \ \  \\\  \ \  \\ \  \ \  \_|\ \ 
+//     \ \____________\ \_______\ \__\\ \__\ \_______\
+//      \|____________|\|_______|\|__| \|__|\|_______|
 
 public class Medicament {
     private String code;
-    private String libelle; 
+    private String libelle;
     public static ArrayList<String> allID = new ArrayList<String>();
-    
-    Medicament(String code, String libelle){
+
+    Medicament(String code, String libelle) {
         try {
-            if(!allID.contains(code)){
+            if (!allID.contains(code)) {
                 this.code = code;
                 this.libelle = libelle;
                 allID.add(code);
-            }else{
+            } else {
                 System.out.println("Il y'a une redondance sur lees identifiants");
             }
         } catch (Exception e) {
@@ -26,6 +33,5 @@ public class Medicament {
     public String getLibelle() {
         return libelle;
     }
-
 
 }
